f3bd655c7b6b3cd8fc11ba02961f79925e33bad4
Abdullah-1360 <abdullah.shah78963@gmail.com>
2025-05-11 11:27:55 +0500

    working db

--- usermodel.js
+++ usermodel.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://new_user:123@cluster0.zhfof.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+mongoose.connect('mongodb+srv://new_user:123@cluster0.zhfof.mongodb.net/users?retryWrites=true&w=majority&appName=Cluster0')
 .then(() => console.log('MongoDB connected successfully'))
 .catch(err => console.error('MongoDB connection error:', err));
 
